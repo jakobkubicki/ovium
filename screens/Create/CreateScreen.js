c2a71ea1a921dc70b1b9899884f5e3b5cb225ff8
Jakob Kubicki <jakobkub@gmail.com>
2023-06-06 16:01:15 -0700

    camera

--- screens/Create/CreateScreen.js
+++ screens/Create/CreateScreen.js
@@ -1,11 +1,27 @@
-
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text, View } from 'react-native';
+import { Camera } from 'expo-camera';
 
 export default function CreateScreen() {
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Create!</Text>
-        </View>
-    );
+  const [hasPermission, setHasPermission] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+  
+  return (
+    <View style={{ flex: 1 }}>
+      <Camera style={{ flex: 1 }} />
+    </View>
+  );
 }
